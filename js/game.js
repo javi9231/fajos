1ef2be5d07938d83db8217d63e5d0d2e94ce8b87
Javi9231 <javi9231@gmail.com>
2019-05-10 02:06:46 +0200

    fajos collider maletin

--- js/game.js
+++ js/game.js
@@ -101,6 +101,7 @@ function create() {
   this.physics.add.collider(fajosEuros, fajosEuros);
   this.physics.add.collider(fajosEuros, platforms);
   this.physics.add.collider(maletin, platforms);
+  this.physics.add.collider(maletin, fajosEuros);
 }
 
 function collectStar(player, star) {
