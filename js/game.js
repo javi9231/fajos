3014f20a1b8095f3fcb3a8d3dc2cde93f19608f8
Javi9231 <javi9231@gmail.com>
2019-05-12 22:49:02 +0200

    zonas

--- js/game.js
+++ js/game.js
@@ -105,7 +105,7 @@ function create() {
 
   // this.physics.add.collider(fajosEuros, fajosEuros);
   this.physics.add.collider(fajosEuros, platforms);
-  //this.physics.add.collider(maletin, platforms);
+  this.physics.add.collider(fajosEuros, zone);
 
 
   // this.physics.add.collider(maletin, fajosEuros);
