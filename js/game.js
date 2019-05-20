4d87000e5c3fa127e0a0ce2685a780d3d4f7f517
jpastor <pastorjavierbemasa@gmail.com>
2019-05-20 13:30:55 +0200

    Scene

--- js/game.js
+++ js/game.js
@@ -1,7 +1,6 @@
-let graphics, score = 200, fajoE, fajosEuros, textoTamanio, rectW, rectH, posRectY, posicionRect;
-let escala = window.devicePixelRatio;
-let totalWidth = window.innerWidth * escala;
-let totalHeight= window.innerHeight * escala;
+// import Phaser from 'phaser';
+// import InicioScene from './inicioScene.js';
+
 
 window.onload = function() {
   let config = {
@@ -22,130 +21,8 @@ window.onload = function() {
         debug: false
       }
     },
-    scene: {
-      preload: preload,
-      create: create,
-      update: update
-    }
+    scene: inicioScene
   };
   window.focus();
-  let game = new Phaser.Game(config);
-  let reloj = new Clock(this, 100, 100, null);
+  const game = new Phaser.Game(config);
 }
-
-
-function preload() {
-  this.load.image('fajoE', 'assets/fajoE.svg');
-}
-
-function create() {
-  this.cameras.main.setBackgroundColor(0xbababa);
-  let fontSize = 18 * escala;
-  preguntaText = this.add.text(40, 20, preguntas[0].pregunta, {
-    fontSize: fontSize,
-    fill: '#000',
-    align: 'center',
-    wordWrap: {
-      width: totalWidth - fontSize
-    }
-  });
-
-  posicionRect = {
-    posX: 50 * escala,
-    posY: totalHeight/4,
-    posXdesplazado: (102 + fontSize)  * escala
-  }
-
-  respuesta(this, posicionRect.posX, preguntas[0].respuestas[0].respuesta, 0xffff00);
-  respuesta(this, posicionRect.posX + posicionRect.posXdesplazado, preguntas[3].respuestas[0].respuesta, 0xff0000);
-
-  cursors = this.input.keyboard.createCursorKeys();
-
-  graphics = this.add.graphics();
-
-  scoreText = this.add.text(totalWidth - 250 * escala,
-  totalHeight - 50 * escala, 'score: ' + score, {
-    fontSize: fontSize,
-    fill: '#000'
-  });
-
-  fajosEuros = this.physics.add.group({
-    key: 'fajoE',
-    repeat: (score / 20) - 1,
-    setXY: {
-      x: totalWidth - posicionRect.posXdesplazado ,
-      y: posicionRect.posY
-    }
-  });
-
-  fajosEuros.children.iterate(fajo => {
-    fajo.setInteractive({
-      draggable: true
-    });
-    fajo.setScale(escala/2);
-    fajo.on('drag', function(pointer, dragX, dragY) {
-      this.x = dragX;
-      this.y = dragY;
-    });
-  });
-  //  The same as above, but uses a method signature to declare it (shorter, and compatible with GSAP syntax)
-   let timedEvent = this.time.delayedCall(60000, tiempoCumplido, [], this);
-}
-
-function tiempoCumplido(){
-  console.log('Tiempo finalizado');
-}
-
-function colorearFajos (scene, zonaX, zonaY, rW, rH, color){
-  let within = scene.physics.overlapRect(zonaX, zonaY, rW, rH, true, true);
-  within.forEach(function(body) {
-    body.gameObject.setTint(color);//.destroy();
-  });
-}
-
-
-function checkOriention(orientation) {
-  if (orientation === Phaser.Scale.PORTRAIT) {
-    graphics.alpha = 0.2;
-    console.log('PORTRAIT');
-    //text.setVisible(true);
-  } else if (orientation === Phaser.Scale.LANDSCAPE) {
-    graphics.alpha = 1;
-    console.log('LANDSCAPE');
-    //text.setVisible(false);
-  }
-}
-
-  function respuesta(scene, containerX, respuesta, rectColor){
-    textoTamanio = 18 * escala;
-    rectW = rectH = 100 * escala;
-
-    posRectX = rectW / 2;
-    posRectY = rectH / 2;
-
-    let posXrespuestaTxt =  (posRectX - respuesta.length * 32 /
-      (respuesta.length ))/ window.devicePixelRatio;
-    // let posYrespuestaTxt = respuesta.length > 10? -100 : -20;
-    let posYrespuestaTxt =  posRectY * 2 + textoTamanio;
-
-    let respuestaText = scene.add.text(posXrespuestaTxt, posYrespuestaTxt, respuesta, {
-      fontSize: textoTamanio,
-      fill: '#000',
-      align: 'center',
-      wordWrap: {
-        width: rectW
-      }
-    });
-    let rect = scene.add.rectangle(posRectX, posRectY, rectW, rectH).setStrokeStyle(10, rectColor);
-    var container = scene.add.container(containerX, posicionRect.posY, [respuestaText, rect]);
-  }
-
-  function update() {
-
-    fajosEuros.children.iterate(fajo => {
-        fajo.clearTint();//(0xffffff);
-    });
-
-    colorearFajos(this, posicionRect.posX, posicionRect.posY, rectW, rectH, 0xffff00);
-    colorearFajos(this, posicionRect.posX + posicionRect.posXdesplazado, posicionRect.posY, rectW, rectH, 0xff0000);
-  }
---
+++ js/inicioScene.js
@@ -0,0 +1,122 @@
+// import fajoBilletes from "../assets/fajoE.svg";
+// import MedidorTiempo from "../../js/object/MedidorTiempo.js";
+
+class inicioScene extends Phaser.Scene {
+  constructor() {
+    super('Juego');
+    this.score = 200;
+    this.escala = window.devicePixelRatio;
+    this.totalWidth = window.innerWidth * this.escala;
+    this.totalHeight= window.innerHeight * this.escala;
+  }
+  preload(){
+    this.load.image('fajoE', "./assets/fajoE.svg");
+  }
+  create() {
+    this.cameras.main.setBackgroundColor(0xbababa);
+
+      // this.medidorView = this.add.container();
+      // this.medidorTiempo = new MedidorTiempo(this, this.medidorView,
+      //   'MedidorTiempo', gameOptions);
+
+      this.fontSize = 18 * this.escala;
+      let preguntaText = this.add.text(40, 20,
+        cuestionario[0].preguntas[0].pregunta, {
+        fontSize: this.fontSize, //'40px',
+        fill: '#000',
+        align: 'center',
+        wordWrap: {
+          width: this.totalWidth - this.fontSize
+        }
+      });
+
+      this.posicionRect = {
+        posX: 50 * this.escala,
+        posY: this.totalHeight / 4,
+        posXdesplazado: (100 + this.fontSize) * this.escala
+      }
+
+      this.respuesta(this, this.posicionRect.posX, cuestionario[0].preguntas[0].respuestas[0], 0xffff00);
+      this.respuesta(this, this.posicionRect.posX + this.posicionRect.posXdesplazado, cuestionario[0].preguntas[0].respuestas[1], 0xff0000);
+
+      this.graphics = this.add.graphics();
+
+      this.scoreText = this.add.text(this.totalWidth - 250 * this.escala,
+        this.totalHeight - 50 * this.escala, 'score: ' + this.score, {
+          fontSize: this.fontSize,
+          fill: '#000'
+        });
+
+      this.fajosEuros = this.physics.add.group({
+        key: 'fajoE',
+        repeat: (this.score / 20) - 1,
+        setXY: {
+          x: this.totalWidth - this.posicionRect.posXdesplazado,
+          y: this.posicionRect.posY
+        }
+      });
+
+      this.fajosEuros.children.iterate(fajo => {
+        fajo.setInteractive({
+          draggable: true
+        });
+        fajo.setScale(this.escala / 2);
+        fajo.on('drag', function(pointer, dragX, dragY) {
+          this.x = dragX;
+          this.y = dragY;
+        });
+      });
+
+  }
+  colorearFajos(scene, zonaX, zonaY, rW, rH, color) {
+    let within = scene.physics.overlapRect(zonaX, zonaY, rW, rH, true, true);
+    within.forEach(function(body) {
+      body.gameObject.setTint(color); //.destroy();
+    });
+  }
+
+
+  checkOriention(orientation) {
+    if (orientation === Phaser.Scale.PORTRAIT) {
+      graphics.alpha = 0.2;
+      console.log('PORTRAIT');
+      //text.setVisible(true);
+    } else if (orientation === Phaser.Scale.LANDSCAPE) {
+      graphics.alpha = 1;
+      console.log('LANDSCAPE');
+      //text.setVisible(false);
+    }
+  }
+
+  respuesta(scene, containerX, respuesta, rectColor){
+    this.rectW = this.rectH = 100 * this.escala;
+
+    this.posRectX = this.rectW / 2;
+    this.posRectY = this.rectH / 2;
+
+    let posXrespuestaTxt = (this.posRectX - respuesta.length * 32 /
+      respuesta.length )/ this.escala - 25;
+
+    let posYrespuestaTxt =  this.posRectY * 2 + this.fontSize;
+
+    let respuestaText = scene.add.text(posXrespuestaTxt, posYrespuestaTxt, respuesta, {
+      fontSize: this.fontSize,
+      fill: '#000',
+      align: 'center',
+      wordWrap: {
+        width: this.rectW
+      }
+    });
+    let rect = scene.add.rectangle(this.posRectX, this.posRectY, this.rectW, this.rectH).setStrokeStyle(10, rectColor);
+    var container = scene.add.container(containerX, this.posicionRect.posY, [respuestaText, rect]);
+  }
+
+  update(){
+    this.fajosEuros.children.iterate(fajo => {
+      fajo.clearTint(); //(0xffffff);
+    });
+
+    this.colorearFajos(this, this.posicionRect.posX, this.posicionRect.posY, this.rectW, this.rectH, 0xffff00);
+    this.colorearFajos(this, this.posicionRect.posX + this.posicionRect.posXdesplazado, this.posicionRect.posY, this.rectW, this.rectH, 0xff0000);
+  }
+}
